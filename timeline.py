5af4dc1be29ecaedefda928dc5186ff4e5826a12
OskarSchnappauf <oskar.schnappauf@gmail.com>
2022-09-19 18:41:07 +0200

    some updates

--- timeline.py
+++ timeline.py
@@ -9,23 +9,23 @@ st.set_page_config(page_title='CV Oskar Schnappauf', page_icon=im, layout="wide"
 #with open( "style.css" ) as css:
 #    st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
 
-image = Image.open('IMG_7274k_colour_round_cut.png')
+#image = Image.open('IMG_7274k_colour_round_cut.png')
 #st.sidebar.image(image, width=None)
 #st.sidebar.header('Oskar Schnappauf, Ph.D, FACMG')
-st.header('CURRICULUM VITAE - OSKAR SCHNAPPAUF, PHD, FACMG')
-col1, col2 = st.columns(2)
+#st.header('CURRICULUM VITAE - OSKAR SCHNAPPAUF, PHD, FACMG')
+#col1, col2 = st.columns(2)
 
-with col1:
-   st.image(image, width=500)
-   st.subheader('CLINICAL MOLECULAR GENETICIST')
-
-with col2:
-   st.header("Contact")
-   st.subheader('+49 157 88224454')
-   st.subheader('Fedderstrasse 4')
-   st.subheader('79106 Freiburg im Breisgau')
-   st.subheader('oskar.schnappauf@gmail.com')
-   st.subheader('American and German Citizienship')
+# with col1:
+#    st.image(image, width=500)
+#    st.subheader('CLINICAL MOLECULAR GENETICIST')
+#
+# with col2:
+#    st.header("Contact")
+#    st.subheader('+49 157 88224454')
+#    st.subheader('Fedderstrasse 4')
+#    st.subheader('79106 Freiburg im Breisgau')
+#    st.subheader('oskar.schnappauf@gmail.com')
+#    st.subheader('American and German Citizienship')
 
 #st.header('Professional - OSKAR SCHNAPPAUF')
 
